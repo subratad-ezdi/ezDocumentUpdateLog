2914bc342f876fca7041fd541447e0cdba9fa026
Subrata Das <subrata.d@ezdi.us>
2019-05-13 15:15:29 +0530

    Added main java file

---
+++ src/main/java/com/ezdi/ezDocumentUpdateLog/ezDocumentUpdateLog.java
@@ -0,0 +1,11 @@
+package com.ezdi.ezDocumentUpdateLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ezDocumentUpdateLog {
+    public static void main(String args[]){
+        SpringApplication.run(ezDocumentUpdateLog.class , args);
+    }
+}
